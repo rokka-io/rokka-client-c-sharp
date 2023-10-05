1948ff6207cca41e53da768aca7a6621133ccf2f
Benoit Potty <benoit.potty@liip.ch>
2023-10-05 10:54:06 +0200

    Clean code

--- rokka-client-c-sharp.tests/Factories/RokkaResponseFactoryTests.cs
+++ rokka-client-c-sharp.tests/Factories/RokkaResponseFactoryTests.cs
@@ -6,7 +6,7 @@ namespace rokka_client_c_sharp.tests.Factories;
 
 public class RokkaResponseFactoryTests
 {
-    private const string SUCCESS_REASON_PHRASE = "OK";
+    private const string SuccessReasonPhrase = "OK";
     private static HttpResponseMessage CreateSuccessResponseMessage()
     {
         const string responseString = "{\"total\":1,\"items\":[{\"hash\":\"07c68ab9e6718b8f23f5e797b3517f22def4219c\",\"short_hash\":\"07c68a\",\"binary_hash\":\"b1c5e43ddb26379491722e928f5bb50d6cd121eb\",\"created\":\"2023-10-03T15:02:50+00:00\",\"name\":\"38b3d4d5bc2c48a59649b26cf25df3c9a8a952c418e84e7b815402b5795b9f9c.jpg\",\"mimetype\":\"image/jpeg\",\"format\":\"jpg\",\"size\":2483460,\"width\":5092,\"height\":3819,\"organization\":\"rokka\",\"link\":\"/sourceimages/rokka/07c68ab9e6718b8f23f5e797b3517f22def4219c\",\"protected\":false,\"locked\":false,\"entropy\":9.01,\"opaque\":true}]}";
@@ -14,7 +14,7 @@ public class RokkaResponseFactoryTests
         return new HttpResponseMessage
         {
             StatusCode = HttpStatusCode.OK,
-            ReasonPhrase = SUCCESS_REASON_PHRASE,
+            ReasonPhrase = SuccessReasonPhrase,
             Content = new StringContent(responseString)
         };
     }
@@ -27,7 +27,7 @@ public class RokkaResponseFactoryTests
         var response = await new RokkaResponseFactory().BuildRokkaResponse(responseMessage);
         
         Assert.Equal(HttpStatusCode.OK, responseMessage.StatusCode);
-        Assert.Equal(SUCCESS_REASON_PHRASE, response.StatusMessage);
+        Assert.Equal(SuccessReasonPhrase, response.StatusMessage);
         Assert.Equal("OK", response.DetailedMessage);
     }
     
