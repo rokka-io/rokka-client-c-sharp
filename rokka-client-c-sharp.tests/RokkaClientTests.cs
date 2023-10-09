9a6d786f8a3c95876404d68228a091563e150aa9
Benoit Potty <benoit.potty@liip.ch>
2023-10-09 17:04:24 +0200

    Add test on Header sent by the client

---
+++ rokka-client-c-sharp.tests/RokkaClientTests.cs
@@ -0,0 +1,64 @@
+using System.Net;
+using Moq;
+using Moq.Protected;
+using rokka_client_c_sharp.Configuration;
+
+namespace rokka_client_c_sharp.tests;
+
+public class RokkaClientTests
+{
+    private const string Organization = "ROKKA";
+    private const string Key = "ROKKA_KEY";
+    private const string RenderStack = "ROKKA_STACK";
+    private Mock<HttpMessageHandler>? _msgHandler;
+
+    private Mock<HttpMessageHandler> GetMockedHandler(Func<HttpRequestMessage, bool> assertRequest)
+    {
+        var messageHandler = new Mock<HttpMessageHandler>();
+        var protectedMock = messageHandler.Protected();
+        var setupApiRequest = protectedMock.Setup<Task<HttpResponseMessage>>(
+            "SendAsync",
+            ItExpr.Is<HttpRequestMessage>(m => assertRequest(m)),
+            ItExpr.IsAny<CancellationToken>()
+        );
+
+        var apiMockedResponse = setupApiRequest.ReturnsAsync(new HttpResponseMessage()
+        {
+            StatusCode = HttpStatusCode.OK,
+            Content = new StringContent("{}")
+        });
+        
+        apiMockedResponse.Verifiable();
+        return messageHandler;
+    }
+
+    [Fact]
+    public async void GivenAConfiguration_WhenCreateImageSource_ThenCorrectApiKeyIsInTheHeader()
+    {
+        _msgHandler = GetMockedHandler(request => request.Headers.GetValues("Api-Key").FirstOrDefault() == Key);
+        
+        var client = new RokkaClient(new RokkaConfiguration()
+        {
+            Organization = Organization, Key = Key, RenderStack = RenderStack
+        }, _msgHandler.Object );
+
+       await client.CreateSourceImage("file.jpg", Array.Empty<byte>());
+       
+       _msgHandler.VerifyAll();
+    }
+    
+    [Fact]
+    public async void GivenAConfiguration_WhenCreateImageSource_ThenCorrectApiVersionIsInTheHeader()
+    {
+        _msgHandler = _msgHandler = GetMockedHandler(request => request.Headers.GetValues("Api-Version").FirstOrDefault() == "1");
+        
+        var client = new RokkaClient(new RokkaConfiguration()
+        {
+            Organization = Organization, Key = Key, RenderStack = RenderStack
+        }, _msgHandler.Object );
+
+        await client.CreateSourceImage("file.jpg", Array.Empty<byte>());
+       
+        _msgHandler.VerifyAll();
+    }
+}
--- rokka-client-c-sharp/RokkaClient.cs
+++ rokka-client-c-sharp/RokkaClient.cs
@@ -1,4 +1,5 @@
-﻿using rokka_client_c_sharp.Configuration;
+﻿using System.Runtime.CompilerServices;
+using rokka_client_c_sharp.Configuration;
 using rokka_client_c_sharp.Factories;
 using rokka_client_c_sharp.Interfaces;
 
@@ -7,14 +8,23 @@ namespace rokka_client_c_sharp;
 public class RokkaClient : IRokkaClient
 {
     private readonly RokkaConfiguration _configuration;
-    private readonly HttpClient _apiHttpClient = new() { BaseAddress = new Uri("https://api.rokka.io")};
+    private static readonly Uri ApiUri = new("https://api.rokka.io");
+    private readonly HttpClient _apiHttpClient;
     private readonly RokkaResponseFactory _responseFactory;
 
-    
-    public RokkaClient(RokkaConfiguration configuration)
+    private RokkaClient(RokkaConfiguration configuration, HttpClient httpClient)
     {
         _configuration = configuration;
+        _apiHttpClient = httpClient;
         _responseFactory = new();
+    }
+    public RokkaClient(RokkaConfiguration configuration): this(configuration, new HttpClient() {BaseAddress = ApiUri})
+    {
+        _configuration = configuration;
+    }
+    
+    internal RokkaClient(RokkaConfiguration configuration, HttpMessageHandler httpMessageHandler): this(configuration, new HttpClient(httpMessageHandler) {BaseAddress = ApiUri})
+    {
     }
 
     private async Task<RokkaResponse> PerformRequest(HttpRequestMessage request)
