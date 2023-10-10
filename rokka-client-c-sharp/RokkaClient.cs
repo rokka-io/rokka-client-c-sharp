5ea1eed9db1d47c7ea583cbded50cd94c1b5c2f6
Benoit Potty <benoit.potty@liip.ch>
2023-10-10 11:27:27 +0200

    Make SourceImageAPI Lazy

--- rokka-client-c-sharp/RokkaClient.cs
+++ rokka-client-c-sharp/RokkaClient.cs
@@ -6,13 +6,14 @@ namespace rokka_client_c_sharp;
 public class RokkaClient : IRokkaClient
 {
     public static readonly Uri ApiUri = new("https://api.rokka.io");
-    public SourceImageEndpoint SourceImages { get; }
+    private readonly Lazy<SourceImageEndpoint> _sourceImages;
+    public SourceImageEndpoint SourceImages => _sourceImages.Value;
 
     private RokkaClient(RokkaConfiguration configuration, HttpClient httpClient)
     {
         if (configuration is null || !configuration.IsValid) throw new RokkaClientException("Rokka Client configuration is invalid");
         var requestExecutor = new RequestExecutor(configuration, httpClient);
-        SourceImages = new SourceImageEndpoint(configuration, requestExecutor);
+        _sourceImages = new Lazy<SourceImageEndpoint>(() => new SourceImageEndpoint(configuration, requestExecutor));
     }
     public RokkaClient(RokkaConfiguration configuration): this(configuration, new HttpClient {BaseAddress = ApiUri})
     {
@@ -21,4 +22,6 @@ public class RokkaClient : IRokkaClient
     internal RokkaClient(RokkaConfiguration configuration, HttpMessageHandler httpMessageHandler): this(configuration, new HttpClient(httpMessageHandler) {BaseAddress = ApiUri})
     {
     }
+
+
 }
