5fe611fac73b674cd0778b9220bf653110e2f97d
Benoit Potty <benoit.potty@liip.ch>
2023-10-11 10:41:32 +0200

    Add Sample program

---
+++ rokka-client-c-sharp.app/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using rokka_client_c_sharp;
+using rokka_client_c_sharp.Configuration;
+using rokka_client_c_sharp.Models.MetaData;
+
+Console.WriteLine("Hello, World!");
+
+var configuration = new RokkaConfiguration()
+{
+    Organization = Environment.GetEnvironmentVariable("ROKKA_ORGANISATION") ?? string.Empty,
+    Key = Environment.GetEnvironmentVariable("ROKKA_KEY") ?? string.Empty,
+    RenderStack = Environment.GetEnvironmentVariable("ROKKA_RENDER_STACK") ?? string.Empty,
+};
+
+var rokkaClient = new RokkaClient(configuration);
+
+var bytes = File.ReadAllBytes("./sample.jpg");
+
+var metadata = new CreateMetadata
+{
+    MetaUser = new MetaDataUser { { "AltText", "Landscape" } },
+    MetaDynamic = new MetaDataDynamic()
+    {
+        SubjectArea = new Area {X = 10, Y= 10, Width = 100, Height = 100},
+        CropArea = new Area {X = 100, Y = 100, Width = 300, Height = 300},
+        Version = new rokka_client_c_sharp.Models.MetaData.Version() { Text = "v0.0.2"}
+    },
+    Options = new MetaDataOptions()
+    {
+        Protected = true
+    }
+};
+
+var options = new CreateOptions()
+{
+    OptimizeSource = true
+};
+
+var response = await rokkaClient.SourceImages.Create("sample.jpg", bytes, metadata, options);
+
+Console.WriteLine(response.DetailedMessage);
